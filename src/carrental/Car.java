b557673325a0931b37478b93dc7f29d86a0f1c7f
frankgzhang <frankzhang0131@gmail.com>
2026-09-19 21:45:01 -0400

    Set up constructor and getter/setter methods for Car

--- src/carrental/Car.java
+++ src/carrental/Car.java
@@ -1,4 +1,29 @@
 package carrental;
 
 public class Car {
+    private final String id;
+    private final CarType type;
+    private boolean available;
+
+    public Car(String id, CarType type) {
+        this.id = id;
+        this.type = type;
+        this.available = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
