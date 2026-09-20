3d8ab6c75ab0f07ebd2a4ad89c859f2e5c40bf89
frankgzhang <frankzhang0131@gmail.com>
2026-09-20 00:11:51 -0400

    Create constructor and getters/setters for Reservation class

--- src/carrental/Reservation.java
+++ src/carrental/Reservation.java
@@ -1,4 +1,40 @@
 package carrental;
 
+import java.time.LocalTime;
+
 public class Reservation {
+    private final String id;
+    private final Car car;
+
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+    private final LocalTime cleanUpTime;
+
+    public Reservation(String id, Car car, LocalTime startTime, LocalTime endTime, LocalTime cleanUpTime) {
+        this.id = id;
+        this.car = car;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.cleanUpTime = cleanUpTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public LocalTime getCleanUpTime() {
+        return cleanUpTime;
+    }
 }
